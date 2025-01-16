04da3ed3d36f89f7253b311133d5c426afc593d6
Andrii Kriachko <science4kettle@gmail.com>
2025-01-16 12:47:08 +0000

    created file open

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -2,5 +2,5 @@
 
 
 def get_sorted_note_ids_generator(notes_generator):
-    for note in notes_generator():
+    for note in notes_generator:
         print(note)
--- src/tmp.py
+++ src/tmp.py
@@ -1,12 +1,12 @@
 import csv
 
-from .lib import get_sorted_note_ids_generator
+from lib import get_sorted_note_ids_generator
 
 
 def get_notes_generator(fpath):
-    with open(fpath) as f:
+    with open(fpath, encoding='utf-8') as f:
         reader = csv.reader(f)
         for row in reader:
             yield row
 
-get_sorted_note_ids_generator(get_notes_generator)
+get_sorted_note_ids_generator(get_notes_generator('/workspace/src/deck.csv'))
